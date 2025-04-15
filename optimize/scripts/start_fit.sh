b4dc48d2860bfe4f6c420abfb56f7448c77e8dd8
Yifan Chen <cyifan@sdfiana008.sdf.slac.stanford.edu>
2025-04-15 00:02:19 -0700

    update fit script, minor details

--- optimize/scripts/start_fit.sh
+++ optimize/scripts/start_fit.sh
@@ -2,7 +2,8 @@
 
 #SBATCH --partition=ampere
 
-#SBATCH --account=mli:cider-ml
+##SBATCH --account=mli:cider-ml
+#SBATCH --account=neutrino:dune-ml
 ##SBATCH --account=neutrino:cider-nu
 ##SBATCH --account=neutrino:ml-dev
 
@@ -12,7 +13,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16g
 #SBATCH --gpus-per-node=a100:1
-#SBATCH --time=3:00:00
+#SBATCH --time=5:00:00
 #SBATCH --array=1,2,3,4,5
 
 #BASE DECLARATIONS
@@ -23,22 +24,24 @@ fi
 
 TARGET_SEED=$SLURM_ARRAY_TASK_ID
 PARAMS=optimize/scripts/param_list.yaml
-BATCH_SIZE=4000
+BATCH_SIZE=2000
 ITERATIONS=15000
 MAX_CLIP_NORM_VAL=1
 #MAX_CLIP_NORM_VAL=0.8
 DATA_SEED=1
 #LOSS=SDTW
 LOSS=chamfer_3d
+SEED_STRATEGY=different
+SAMPLING_STEP=0.0005 #0.0005 #0.005 cm
 
-### proton 1E5 events
-#INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
-#INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
+## proton 1E5 events
+INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
+INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
 
-### proton 5000 events
-INPUT_FILE_TGT=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000.h5
-INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000.h5
-#INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000_NIST_dEdx_cubic.h5
+#### proton 5000 events
+#INPUT_FILE_TGT=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000.h5
+#INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000.h5
+##INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000_NIST_dEdx_cubic.h5
 
 ### muon 100 events
 #INPUT_FILE_TGT=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim-output.h5
@@ -66,10 +69,10 @@ apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m op
     --track_len_sel 2 \
     --max_abs_costheta_sel 0.966 \
     --min_abs_segz_sel 15. \
-    --no-noise-guess \
+    --no-noise \
     --data_seed ${DATA_SEED} \
     --num_workers 0 \
-    --out_label p_5E3_6par_noise_tgt_grad_clip${MAX_CLIP_NORM_VAL}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_target_${UUID} \
+    --out_label p_1E5_6par_no_noise_e_sampling_${SAMPLING_STEP}cm_seed_strategy_${SEED_STRATEGY}_grad_clip${MAX_CLIP_NORM_VAL}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_target_${UUID} \
     --test_name fit_noise \
     --seed ${TARGET_SEED} \
     --optimizer_fn Adam \
@@ -81,11 +84,12 @@ apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m op
     --lr_kw '{"decay_rate" : 0.99}' \
     --track_z_bound 28 \
     --max_clip_norm_val ${MAX_CLIP_NORM_VAL} \
-    --electron_sampling_resolution 0.005 \
+    --electron_sampling_resolution ${SAMPLING_STEP} \
     --number_pix_neighbors 0 \
     --signal_length 191 \
     --mode 'parametrized' \
     --loss_fn ${LOSS} \
+    --sim_seed_strategy ${SEED_STRATEGY} \
     #--print_input
     # --loss_fn SDTW \
     # --lut_file /home/pgranger/larnd-sim/jit_version/original/build/lib/larndsim/bin/response_44.npy
