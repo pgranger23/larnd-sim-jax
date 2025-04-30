6284e924b83203cea7c151f200949d735f263c5b
Yifan Chen <cyifan@sdfiana007.sdf.slac.stanford.edu>
2025-04-30 14:35:27 -0700

    update the job script

--- optimize/scripts/start_fit.sh
+++ optimize/scripts/start_fit.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition=ampere
 
-#SBATCH --account=mli:cider-ml
-##SBATCH --account=neutrino:dune-ml
+##SBATCH --account=mli:cider-ml
+#SBATCH --account=neutrino:dune-ml
 ##SBATCH --account=neutrino:cider-nu
 ##SBATCH --account=neutrino:ml-dev
 
@@ -13,8 +13,10 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=128g
 #SBATCH --gpus-per-node=a100:1
-#SBATCH --time=5:00:00
+#SBATCH --time=6:00:00
 #SBATCH --array=1,2,3,4,5
+##SBATCH --time=1:00:00
+##SBATCH --array=1
 
 #BASE DECLARATIONS
 
@@ -25,14 +27,14 @@ fi
 TARGET_SEED=$SLURM_ARRAY_TASK_ID
 PARAMS=optimize/scripts/param_list.yaml
 BATCH_SIZE=2000
-ITERATIONS=15000
+ITERATIONS=10000
 MAX_CLIP_NORM_VAL=1
 #MAX_CLIP_NORM_VAL=0.8
 DATA_SEED=1
 #LOSS=SDTW
 LOSS=chamfer_3d
 SEED_STRATEGY=different
-SAMPLING_STEP=0.0005 #0.0005 #0.005 cm
+SAMPLING_STEP=0.005 #0.0005 #0.005 cm
 
 ### proton 1E5 events
 #INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
@@ -60,27 +62,31 @@ nvidia-smi
 
 # apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m optimize.example_run \
 # apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} nsys profile --capture-range=cudaProfilerApi --cuda-graph-trace=node --capture-range-end=stop python3 -m optimize.example_run \
-apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m optimize.example_run \
+#apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} pip install -e .; python3 -m optimize.example_run \
+#apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m optimize.example_run \
+apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} /bin/bash -c "
+pip3 install .; \
+python3 -m optimize.example_run \
     --data_sz -1 \
     --max_nbatch -1 \
     --params ${PARAMS} \
     --input_file_sim ${INPUT_FILE_SIM} \
     --input_file_tgt ${INPUT_FILE_TGT} \
+    --fit_type chain \
     --track_len_sel 2 \
     --max_abs_costheta_sel 0.966 \
     --min_abs_segz_sel 15. \
-    --no-noise-guess \
+    --no-noise \
     --data_seed ${DATA_SEED} \
-    --out_label p_5000_6par_noise_tgt_e_sampling_${SAMPLING_STEP}cm_seed_strategy_${SEED_STRATEGY}_grad_clip${MAX_CLIP_NORM_VAL}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_target_${UUID} \
+    --out_label p_5000_6par_match_z_diff_curr_no_noise_e_sampling_${SAMPLING_STEP}cm_seed_strategy_${SEED_STRATEGY}_grad_clip${MAX_CLIP_NORM_VAL}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_${UUID} \
     --test_name fit_noise \
     --seed ${TARGET_SEED} \
     --optimizer_fn Adam \
     --random_ntrack \
     --iterations ${ITERATIONS} \
     --max_batch_len ${BATCH_SIZE} \
-    --link-vdrift-eField \
     --lr_scheduler exponential_decay \
-    --lr_kw '{"decay_rate" : 0.99}' \
+    --lr_kw '{\"decay_rate\" : 0.99}' \
     --track_z_bound 28 \
     --max_clip_norm_val ${MAX_CLIP_NORM_VAL} \
     --electron_sampling_resolution ${SAMPLING_STEP} \
@@ -89,6 +95,8 @@ apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m op
     --mode 'parametrized' \
     --loss_fn ${LOSS} \
     --sim_seed_strategy ${SEED_STRATEGY} \
+    --chamfer_match_z \
+    --diffusion_in_current_sim \
     #--print_input
     # --loss_fn SDTW \
     # --lut_file /home/pgranger/larnd-sim/jit_version/original/build/lib/larndsim/bin/response_44.npy
@@ -98,6 +106,6 @@ apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m op
     # --mode 'parametrized'
     # --profile_gradient 
     # --loss_fn space_match
-
+"
 
 # nsys profile --capture-range=cudaProfilerApi --cuda-graph-trace=node --capture-range-end=stop-shutdown python3 -m optimize.example_run \
