c686715dbfaff85453c93fd8781a4dbdeceee50e
Pierre Granger <pierre.granger@cern.ch>
2025-05-05 02:38:46 -0700

    Config update

--- optimize/simulate_file.sh
+++ optimize/simulate_file.sh
@@ -1,6 +1,30 @@
-INPUT_FILE=/home/pgranger/larnd-sim-latest/edepsim-output.h5
+#!/bin/bash
+
+#SBATCH --partition=ampere
+
+##SBATCH --account=mli:nu-ml-dev
+##SBATCH --account=mli:cider-ml
+##SBATCH --account=neutrino:cider-nu
+#SBATCH --account=neutrino:dune-ml
+##SBATCH --account=neutrino:ml-dev
+
+#SBATCH --job-name=diffsim_jac
+#SBATCH --output=logs/scan/job-%j.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16g
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --time=4:00:00
+#SBATCH --array=0
+
+SIF_FILE=/sdf/group/neutrino/pgranger/larnd-sim-jax.sif
+
+INPUT_FILE=/sdf/group/neutrino/pgranger/larndsim-jax/data/mixed_sample/edepsim-output.h5
 
 # nsys profile --capture-range=cudaProfilerApi --cuda-graph-trace=node --capture-range-end=stop-shutdown
+
+apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} /bin/bash -c "
+pip install .
 python3 -m optimize.simulate \
     --input_file ${INPUT_FILE} \
     --output_file output/output-test.h5 \
@@ -11,3 +35,4 @@ python3 -m optimize.simulate \
     --noise \
     --jac \
     --gpu
+"
