b3148d8f49bb4f8d1e6a6e9b554b13883e339501
Yifan Chen <cyifan@sdfiana008.sdf.slac.stanford.edu>
2025-09-03 00:29:50 -0700

    update the job scripts

--- optimize/scripts/start_fit.sh
+++ optimize/scripts/start_fit.sh
@@ -11,12 +11,10 @@
 #SBATCH --output=logs/fit_noise/job-%j.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=128g
+#SBATCH --mem-per-cpu=32g
 #SBATCH --gpus-per-node=a100:1
-#SBATCH --time=1:00:00
+#SBATCH --time=8:00:00
 #SBATCH --array=1,2,3,4,5
-##SBATCH --time=1:00:00
-##SBATCH --array=4
 
 #BASE DECLARATIONS
 
@@ -26,34 +24,36 @@ fi
 
 TARGET_SEED=$SLURM_ARRAY_TASK_ID
 PARAMS=optimize/scripts/param_list.yaml
-BATCH_SIZE=1000
-ITERATIONS=1000
+BATCH_SIZE=400
+ITERATIONS=10000
 MAX_CLIP_NORM_VAL=1
-#MAX_CLIP_NORM_VAL=0.8
 DATA_SEED=1
-#LOSS=SDTW
-LOSS=chamfer_3d
-SEED_STRATEGY=different
-SAMPLING_STEP=0.005 #0.0005 #0.005 cm
+LOSS=mse_adc
+SEED_STRATEGY=different_epoch
+SAMPLING_STEP=0.01 # cm
 N_NEIGH=4
 MODE="lut"  #"parametrized"
+LR_SCHEDULER=warmup_exponential_decay_schedule
 
-### proton 1E5 events
-#INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
-#INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/dunend_train_prod/prod_mod0_mpvmpr/production_2826844_p_1E5/job_64452404_0001/output_64452404_0001-edepsim.h5
+## true proton
+#INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/diffsim_input/true_proton_edep_2cm.h5
+#INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/diffsim_input/true_proton_edep_2cm.h5
 
-#### proton 5000 events
-#INPUT_FILE_TGT=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000.h5
-#INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000.h5
-##INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim_p_only_active_vol_5000_NIST_dEdx_cubic.h5
+## true stopping muon
+INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/diffsim_input/true_stopping_muon_edep_5cm_vol2cm.h5
+# full truth
+#INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/diffsim_input/true_stopping_muon_edep_5cm_vol2cm.h5
+# 'reco' dE/dx
+INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/diffsim_input/true_stopping_muon_edep_5cm_vol2cm_range_dEdx.h5
 
-## muon 100 events
-INPUT_FILE_TGT=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim-output.h5
-INPUT_FILE_SIM=/sdf/group/neutrino/cyifan/muon-sim/fake_data_S1/edepsim-output.h5
+## true through going muon
+#INPUT_FILE_TGT=/sdf/data/neutrino/cyifan/diffsim_input/true_through_muon_edep_10cm_vol1cm.h5
+#INPUT_FILE_SIM=/sdf/data/neutrino/cyifan/diffsim_input/true_through_muon_edep_10cm_vol1cm.h5
 
 SIF_FILE=/sdf/group/neutrino/pgranger/larnd-sim-jax.sif
 UUID=$(uuidgen)
-LABEL=mu_2par_Ab_kb_n_neigh${N_NEIGH}_mode_${MODE}_diff_curr_no_noise_e_sampling_${SAMPLING_STEP}cm_seed_strategy_${SEED_STRATEGY}_grad_clip${MAX_CLIP_NORM_VAL}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_${UUID}
+#LABEL=stopping_mu_range_dEdx_6par_n_neigh${N_NEIGH}_mode_${MODE}_noise_e_sampling_${SAMPLING_STEP}cm_seed_strategy_${SEED_STRATEGY}_grad_clip${MAX_CLIP_NORM_VAL}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_${UUID}
+LABEL=stopping_mu_range_dEdx_lr_8e-3_6par_noise_n_neigh${N_NEIGH}_mode_${MODE}_e_sampling_${SAMPLING_STEP}cm_seed_strategy_${SEED_STRATEGY}_grad_clip${MAX_CLIP_NORM_VAL}_${LR_SCHEDULER}_bt${BATCH_SIZE}_tgtsd${TARGET_SEED}_dtsd${DATA_SEED}_adam_${LOSS}_${UUID}
 #DECLARATIONS
 
 nvidia-smi
@@ -62,15 +62,12 @@ nvidia-smi
 # export JAX_LOG_COMPILES=1
 
 
-# apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m optimize.example_run \
 # apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} nsys profile --capture-range=cudaProfilerApi --cuda-graph-trace=node --capture-range-end=stop python3 -m optimize.example_run \
-#apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} pip install -e .; python3 -m optimize.example_run \
-#apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} python3 -m optimize.example_run \
 apptainer exec --nv -B /sdf,/fs,/sdf/scratch,/lscratch ${SIF_FILE} /bin/bash -c "
 pip3 install .; \
 python3 -m optimize.example_run \
     --data_sz -1 \
-    --max_nbatch -1 \
+    --max_nbatch 200 \
     --params ${PARAMS} \
     --input_file_sim ${INPUT_FILE_SIM} \
     --input_file_tgt ${INPUT_FILE_TGT} \
@@ -79,7 +76,6 @@ python3 -m optimize.example_run \
     --track_len_sel 2 \
     --max_abs_costheta_sel 0.966 \
     --min_abs_segz_sel 15. \
-    --no-noise \
     --data_seed ${DATA_SEED} \
     --out_label ${LABEL} \
     --test_name fit_noise \
@@ -88,8 +84,6 @@ python3 -m optimize.example_run \
     --random_ntrack \
     --iterations ${ITERATIONS} \
     --max_batch_len ${BATCH_SIZE} \
-    --lr_scheduler exponential_decay \
-    --lr_kw '{\"decay_rate\" : 0.99}' \
     --track_z_bound 28 \
     --max_clip_norm_val ${MAX_CLIP_NORM_VAL} \
     --electron_sampling_resolution ${SAMPLING_STEP} \
@@ -100,9 +94,12 @@ python3 -m optimize.example_run \
     --lut_file ../Data_selection/response_44_v2a_full_tick.npz \
     --loss_fn ${LOSS} \
     --sim_seed_strategy ${SEED_STRATEGY} \
-    --diffusion_in_current_sim \
     --clip_from_range \
-    --live_selection
+    --lr_scheduler ${LR_SCHEDULER} \
+    --lr_kw '{\"decay_rate\" : 0.98, \"init_value\" : 0, \"warmup_steps\": 200}' \
+    #--lr_scheduler exponential_decay \
+    #--lr_kw '{\"decay_rate\" : 0.99}' \
+    #--live_selection
     #--chamfer_match_z \
     #--print_input
     # --loss_fn SDTW \
