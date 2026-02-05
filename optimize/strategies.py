2cbe5b8b93953bd0894cd390ad1127b632137913
Pierre Granger <pierre.granger@cern.ch>
2026-02-05 07:53:35 -0800

    Fixing the fix

--- optimize/strategies.py
+++ optimize/strategies.py
@@ -53,6 +53,7 @@ class LUTProbabilisticSimulation(SimulationStrategy):
             'pixel_plane': pixel_plane,   # Needed for z-coordinate calculation
             'event': event,
             'unique_pixels': unique_pixels, 
+            'hit_pixels': unique_pixels,
             'wfs': wfs
         }
 
@@ -275,7 +276,7 @@ class ProbabilisticLossStrategy(LossStrategy):
         
         # Step 1: Match target hits to predicted pixel distributions
         target_pixel_ids = target['pixel_id']
-        sim_unique_pixels = prediction['pixel_id']
+        sim_unique_pixels = prediction['unique_pixels']
         
         # Find indices of target pixels in simulation output (unique_pixels is sorted)
         pixel_indices = jnp.searchsorted(sim_unique_pixels, target_pixel_ids)
